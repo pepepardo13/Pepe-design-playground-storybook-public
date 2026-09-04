fe31874e878467e029ffbae536def669b901864c
Pepe <pepe@local>
2026-09-04 13:05:40 -0600

    Deploy Storybook: add Pricing Teams prototype (1-5 member selector A/B/C, team tiers, AI-models marquee)

---
+++ assets/PricingPageV2.stories-BdgUEt6P.js
@@ -0,0 +1,6 @@
+import{j as e}from"./iframe-D7Bg9DOm.js";import{P as o}from"./PricingPageV2-D-5yG6vr.js";import{B as t}from"./Bleed-B8ZmGHcT.js";import"./preload-helper-PPVm8Dsz.js";import"./PricingPageVariant3aNew.module-gNmdIldr.js";import"./Icon-BL1WNJ4m.js";import"./properties-ByAOh3LD.js";import"./minimax-BYdkqfOb.js";const l={title:"Prototype / Pricing Page 2.0",parameters:{layout:"fullscreen"}},r={name:"Pricing Page 2.0",render:()=>e.jsx(t,{uniform:"3x",children:e.jsx(o,{})})};r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:`{
+  name: "Pricing Page 2.0",
+  render: () => <Bleed uniform="3x">
+      <PricingPageV2 />
+    </Bleed>
+}`,...r.parameters?.docs?.source}}};const P=["Default"];export{r as Default,P as __namedExportsOrder,l as default};
--- assets/formatter-EIJCOSYU-Dh5Mc6xV.js
+++
@@ -1,1 +0,0 @@
-import{I as i,x as a,G as m}from"./iframe-aZEJA_Eb.js";import"./preload-helper-PPVm8Dsz.js";var t=a(m(),1),p=(0,t.default)(2)(async(e,r)=>e===!1?r:i(r));export{p as formatter};
